b6a9d79df3a8262408397d81fa423ec0cb961fcb
skjanani <41111202+skjanani@users.noreply.github.com>
2018-08-15 11:05:41 +0530

    Add files via upload

---
+++ ModuloDivison.java
@@ -0,0 +1,22 @@
+package janani;
+import java.util.Scanner;
+public class ModuloDivison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//int n,n1,a,b;
+double n,n1;
+double a=0;
+System.out.println("enter the number");
+Scanner ja=new Scanner(System.in);
+n=ja.nextDouble();
+n1=ja.nextDouble();
+/*a=n%n1;
+b=n/n1;
+System.out.println(a);
+System.out.println(b);*/
+a=n*n1;
+System.out.println(a);
+	}
+
+}
