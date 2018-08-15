b02a02fb90cbc324269ab0836464aa806033656e
skjanani <41111202+skjanani@users.noreply.github.com>
2018-08-15 11:18:48 +0530

    Create double2.java

---
+++ double2.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		double n,n1,a;
+		System.out.println("enter the number");
+		Scanner ja=new Scanner(System.in);
+		n=ja.nextDouble();
+		n1=ja.nextDouble();
+		a=n*n1;
+		System.out.println(a);
+	}
+}
